7be39a924e3d6b34404b954753d5e2192b4399cd
Matheus Romano <99839311+cooingcy@users.noreply.github.com>
2024-02-22 20:34:06 -0300

    Exercícios 16/02 PAM

---
+++ Exercicios/Exercicios_16.02/Exercicio_2/src/Fundamentos/Main.kt
@@ -0,0 +1,8 @@
+package Fundamentos
+
+fun main(args: Array<String>) {
+    var a: Int
+    var b = 2 // inferido
+    a = 10
+    print(a+b)
+}
---
+++ Exercicios/Exercicios_16.02/Exercicio_5/src/Fundamentos/Main.kt
@@ -0,0 +1,6 @@
+package Fundamentos
+
+fun main(args: Array<String>) {
+    val aprovados = listOf("João", "Maria", "Pedor")
+    print("O primeiro colocado foi ${aprovados[0]}")
+}
